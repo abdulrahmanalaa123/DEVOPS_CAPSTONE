00c2d596ea876e268db5e720ffca2e74badf2856
ZiadTarek10 <ziadtarekmhamed32@gmail.com>
2025-04-25 18:55:35 +0300

    added sql and redis pods via helm chart and added jenkins pipeline groovy script

--- terraform/modules/helm/main.tf
+++ terraform/modules/helm/main.tf
@@ -11,3 +11,26 @@ resource "helm_release" "argocd" {
   values = [file("../argo-cd/values.yaml")]
   depends_on = [var.cluster_name]
 }
+
+resource "helm_release" "mysql" {
+  name       = "my-mysql"
+  namespace  = "default"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mysql"
+
+  values = [
+    file("helm/mysql-values.yaml")
+  ]
+}
+
+resource "helm_release" "redis" {
+  name       = "my-redis"
+  namespace  = "default"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
+
+  values = [
+    file("helm/redis-values.yaml")
+  ]
+}
+
